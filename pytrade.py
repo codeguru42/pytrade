0c91ec6e02ccf46ccfd55d3bd66a3af2ba63d97e
Code Apprentice <codeguru42@gmail.com>
2017-02-09 11:14:04 -0700

    Attempt to implement `get_quote()`

--- pytrade.py
+++ pytrade.py
@@ -1,7 +1,13 @@
+import requests
+
+sandbox_url = 'https://etwssandbox.etrade.com'
 base_url = 'https://etws.etrade.com/'
 
 def get_quote(symbol):
-    pass
+    path = 'market/sandbox/rest/quote/'
+    url = sandbox_url + path + symbol + '.json'
+    response = requests.get(url)
+    return response
 
 def main():
     print(get_quote('SQ'))

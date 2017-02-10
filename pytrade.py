db56456e8c3accd2e82d86f246e70da4bf20e3d5
Code Apprentice <codeguru42@gmail.com>
2017-02-09 18:14:27 -0700

    Implement `get_authorization_url()`

--- pytrade.py
+++ pytrade.py
@@ -5,12 +5,20 @@ import config
 
 sandbox_url = 'https://etwssandbox.etrade.com'
 base_url = 'https://etws.etrade.com/'
+oauth_session = requests_oauthlib.OAuth1Session(config.oauth_consumer_key, config.consumer_secret, callback_uri='oob')
+
 
 def get_request_token():
     path = 'oauth/request_token'
     url = base_url + path
-    oauth = requests_oauthlib.OAuth1Session(config.oauth_consumer_key, config.consumer_secret, callback_uri='oob')
-    return oauth.fetch_request_token(url)
+    return oauth_session.fetch_request_token(url)
+
+
+def get_authorization_url(request_token):
+    url_format = 'https://us.etrade.com/e/t/etws/authorize?key={oauth_consumer_key}&token={oauth_token}'
+    url = url_format.format(oauth_consumer_key=config.oauth_consumer_key, oauth_token=request_token['oauth_token'])
+    return oauth_session.authorization_url(url)
+
 
 def get_quote(symbol):
     path = 'market/sandbox/rest/quote/'
@@ -18,9 +26,12 @@ def get_quote(symbol):
     response = requests.get(url)
     return response
 
+
 def main():
-    print(get_request_token())
+    request_token = get_request_token()
+    print(get_authorization_url(request_token))
     print(get_quote('SQ'))
+
 
 if __name__ == '__main__':
     main()

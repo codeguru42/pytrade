b2521e51298897d989484caf69a192a696178b31
Code Apprentice <codeguru42@gmail.com>
2017-02-09 17:49:58 -0700

    Implement `get_request_token()`

--- pytrade.py
+++ pytrade.py
@@ -1,7 +1,16 @@
 import requests
+import requests_oauthlib
+
+import config
 
 sandbox_url = 'https://etwssandbox.etrade.com'
 base_url = 'https://etws.etrade.com/'
+
+def get_request_token():
+    path = 'oauth/request_token'
+    url = base_url + path
+    oauth = requests_oauthlib.OAuth1Session(config.oauth_consumer_key, config.consumer_secret, callback_uri='oob')
+    return oauth.fetch_request_token(url)
 
 def get_quote(symbol):
     path = 'market/sandbox/rest/quote/'
@@ -10,6 +19,7 @@ def get_quote(symbol):
     return response
 
 def main():
+    print(get_request_token())
     print(get_quote('SQ'))
 
 if __name__ == '__main__':

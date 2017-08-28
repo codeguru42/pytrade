ecf0280fb4c1e43b655d609783014c3a3bfe9961
Code Apprentice <codeguru42@gmail.com>
2017-08-27 23:09:55 -0600

    Get an access token

--- pytrade.py
+++ pytrade.py
@@ -4,17 +4,18 @@ import requests_oauthlib
 import config
 
 sandbox_url = 'https://etwssandbox.etrade.com'
-base_url = 'https://etws.etrade.com/'
+base_url = 'https://etws.etrade.com'
 
 
 class Authorization:
     def __init__(self):
         self.oauth_session = requests_oauthlib.OAuth1Session(config.oauth_consumer_key, config.consumer_secret,
                                                              callback_uri='oob')
+        self.oauth_url = base_url + "/oauth"
 
     def get_request_token(self):
-        path = 'oauth/request_token'
-        url = base_url + path
+        request_token_path = '/request_token'
+        url = self.oauth_url + request_token_path
         return self.oauth_session.fetch_request_token(url)
 
     def get_authorization_url(self):
@@ -22,6 +23,11 @@ class Authorization:
         url_format = 'https://us.etrade.com/e/t/etws/authorize?key={oauth_consumer_key}&token={oauth_token}'
         url = url_format.format(oauth_consumer_key=config.oauth_consumer_key, oauth_token=request_token['oauth_token'])
         return self.oauth_session.authorization_url(url)
+
+    def get_access_token(self, verification_code):
+        access_token_path = '/access_token'
+        url = self.oauth_url + access_token_path
+        return self.oauth_session.fetch_access_token(url, verifier=verification_code)
 
 
 def get_quote(symbol):
@@ -34,6 +40,9 @@ def get_quote(symbol):
 def main():
     auth = Authorization()
     print(auth.get_authorization_url())
+    verification_code = input("Paste verification code here: ")
+    access_token = auth.get_access_token(verification_code)
+    print('access token: ', access_token)
 
 
 if __name__ == '__main__':

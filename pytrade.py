aa4a980e32298a9011115ad24ad599313f3c3eff
Code Apprentice <codeguru42@gmail.com>
2017-08-27 20:11:17 -0600

    Create Authorization class

--- pytrade.py
+++ pytrade.py
@@ -5,19 +5,23 @@ import config
 
 sandbox_url = 'https://etwssandbox.etrade.com'
 base_url = 'https://etws.etrade.com/'
-oauth_session = requests_oauthlib.OAuth1Session(config.oauth_consumer_key, config.consumer_secret, callback_uri='oob')
 
 
-def get_request_token():
-    path = 'oauth/request_token'
-    url = base_url + path
-    return oauth_session.fetch_request_token(url)
+class Authorization:
+    def __init__(self):
+        self.oauth_session = requests_oauthlib.OAuth1Session(config.oauth_consumer_key, config.consumer_secret,
+                                                             callback_uri='oob')
 
+    def get_request_token(self):
+        path = 'oauth/request_token'
+        url = base_url + path
+        return self.oauth_session.fetch_request_token(url)
 
-def get_authorization_url(request_token):
-    url_format = 'https://us.etrade.com/e/t/etws/authorize?key={oauth_consumer_key}&token={oauth_token}'
-    url = url_format.format(oauth_consumer_key=config.oauth_consumer_key, oauth_token=request_token['oauth_token'])
-    return oauth_session.authorization_url(url)
+    def get_authorization_url(self):
+        request_token = self.get_request_token()
+        url_format = 'https://us.etrade.com/e/t/etws/authorize?key={oauth_consumer_key}&token={oauth_token}'
+        url = url_format.format(oauth_consumer_key=config.oauth_consumer_key, oauth_token=request_token['oauth_token'])
+        return self.oauth_session.authorization_url(url)
 
 
 def get_quote(symbol):
@@ -28,9 +32,8 @@ def get_quote(symbol):
 
 
 def main():
-    request_token = get_request_token()
-    print(get_authorization_url(request_token))
-    print(get_quote('SQ'))
+    auth = Authorization()
+    print(auth.get_authorization_url())
 
 
 if __name__ == '__main__':
